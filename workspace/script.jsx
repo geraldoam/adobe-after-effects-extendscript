06e0fcaf563514ab40be6853c0e334eea7b0d196
i686leak <geraldoahnert@gmail.com>
2020-08-21 22:38:20 -0300

    Renomeando e adicionando novo arquivo na workspace.

---
+++ workspace/script.jsx
@@ -0,0 +1,13 @@
+﻿var path = './template.aep';
+var jsonFile = File("./ranking.json");
+
+function loadTemplate(){
+    importOptions.file = new File(path)
+    var item = app.project.importFile(importOptions);
+}
+
+function main(path){
+    loadTemplate(path);
+}
+
+main(path);
