cbc9a2b53c19e565ced7c8536f8089a80bc9761b
gerald0x01 <geraldoahnert@gmail.com>
2020-08-26 19:26:32 -0300

    Removendo comentários desnecessários.

--- workspace/script.jsx
+++ workspace/script.jsx
@@ -3,27 +3,22 @@ var jsonFile = File("./ranking.json");
 
 
 function loadTemplate(path){
-    app.open(new File(path));   // refatorado a forma que abre o projeto, essa eh a forma certa.
+    app.open(new File(path));
 }
 
 function changeVideoTitle(jsonFile){
-
-    // seleciona composicao
     var theComposition = app.project.item(1);
 
-    // seleciona os videos nas layers respectivas
     var theTextLayerOne = theComposition.layers[1];
     var theTextLayerTwo = theComposition.layers[2];
     var theTextLayerThree = theComposition.layers[3];
 
-    // le o arquivo json
     jsonFile.open("r");
     var data = jsonFile.read();
     data = JSON.parse(data);
 
     var videoTitles = [];
     for (var i=0;i < data.length;i++) {
-        // refatorado
         videoTitles.push(data[i].videoName);
     }
 
