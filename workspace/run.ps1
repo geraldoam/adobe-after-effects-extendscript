bf3728bde4683a81df4d9db526b9df2c88405c1c
gerald0x01 <geraldoahnert@gmail.com>
2020-08-27 16:20:21 -0300

    Alterando path defeituoso.

--- workspace/run.ps1
+++ workspace/run.ps1
@@ -1,13 +1,17 @@
-# Script
+# # Script
 $exe_script = 'C:\Program Files\Adobe\Adobe After Effects 2020\Support Files\AfterFX.exe'
 $path_script = 'C:\Users\geral\Desktop\Freela\After\ae-extendscript\workspace\script.jsx'
 
-& "$exe_script" -r $path_script
+& "$exe_script" -noui -r $path_script
+
+
+# # Sleep
+# Start-Sleep -s 30
 
 
 # Render
 $exe_render = 'C:\Program Files\Adobe\Adobe After Effects 2020\Support Files\aerender.exe'
-$path_project = 'C:\Users\geral\Desktop\Freela\After\ae-extendscript\workspace\template.aep'
+$path_project = 'C:\Users\geral\Desktop\Freela\After\ae-extendscript\workspace\projects\final_project.aep'
 $comp_name = 'Composition'
 $output_path = 'C:\Users\geral\Desktop\Freela\After\ae-extendscript\workspace\output\final_project.avi'
 
